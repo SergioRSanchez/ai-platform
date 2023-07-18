6027e4fdc66c3f4b65a5b0f2d65d5cf5d60829bf
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-07-18 10:42:37 -0300

    feat: create repository with changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+'use client'
+
+import { MagicWand, Code, ImageSquare, PlayCircle, MusicNotesSimple, PaperPlaneRight, PlusCircle, ArrowRight } from '@phosphor-icons/react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div 
+      className='flex flex-col items-center justify-center gap-10 py-9 sm:py-12 sm:px-12'
+      style={{
+        borderRadius: '2rem',
+        border: '1px solid var(--gray-04, #334155)',
+        background: 'rgba(2, 6, 23, 0.80)',
+      }}
+    >
+      <div className='flex flex-col gap-10'>
+        {/* TITLE */}
+        <div className='flex flex-col justify-center items-center gap-3 sm:gap-4'>
+          <div className='p-3 w-fit' style={{borderRadius: '1rem', border: '1px solid var(--gray-05, #1E293B)', background: 'var(--gray-06, #0F172A)'}}>
+            <MagicWand className='w-10 h-10 text-gray-2'/>
+          </div>
+          <h1 className='text-gray-1 text-center text-3xl font-bold select-none'>Experimente o poder da Inteligência Artificial</h1>
         </div>
+        
+        {/* OPTIONS */}
+        <div className='flex flex-col gap-2 sm:gap-3 sm:items-center'>
+          {/* OPTION */}
+          <div 
+            className='flex items-center justify-between p-2 group hover:scale-105 transition-all duration-300 sm:min-w-[50%]'
+            style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}
+          >
+            {/* LEFT */}
+            <div className='flex items-center gap-6'>
+              <div className='p-3 w-fit group-hover:brightness-150 transition-all duration-300' style={{borderRadius: '1rem', border: '1px solid var(--gray-05, #1E293B)', background: 'var(--gray-06, #0F172A)'}}>
+                <Code className='text-gray-3 h-8 w-8'/>
+              </div>
+              <span className='text-gray-2 sm:text-lg font-bold select-none'>Gerador de códigos</span>
+            </div>
+            {/* RIGHT */}
+            <div className='p-3'>
+              <ArrowRight className='text-gray-3 w-8 h-8 group-hover:text-gray-2 transition-all duration-300' />
+            </div>
+          </div>
+
+          <div 
+            className='flex items-center justify-between p-2 group hover:scale-105 transition-all duration-300 sm:min-w-[50%]'
+            style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}
+          >
+            {/* LEFT */}
+            <div className='flex items-center gap-6'>
+              <div className='p-3 w-fit group-hover:brightness-150 transition-all duration-300' style={{borderRadius: '1rem', border: '1px solid var(--gray-05, #1E293B)', background: 'var(--gray-06, #0F172A)'}}>
+                <ImageSquare className='text-gray-3 h-8 w-8'/>
+              </div>
+              <span className='text-gray-2 sm:text-lg font-bold select-none'>Edição de foto</span>
+            </div>
+            {/* RIGHT */}
+            <div className='p-3'>
+              <ArrowRight className='text-gray-3 w-8 h-8 group-hover:text-gray-2 transition-all duration-300' />
+            </div>
+          </div>
+          
+          <div 
+            className='flex items-center justify-between p-2 group hover:scale-105 transition-all duration-300 sm:min-w-[50%]'
+            style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}
+          >
+            {/* LEFT */}
+            <div className='flex items-center gap-6'>
+              <div className='p-3 w-fit group-hover:brightness-150 transition-all duration-300' style={{borderRadius: '1rem', border: '1px solid var(--gray-05, #1E293B)', background: 'var(--gray-06, #0F172A)'}}>
+                <PlayCircle className='text-gray-3 h-8 w-8'/>
+              </div>
+              <span className='text-gray-2 sm:text-lg font-bold select-none'>Geração de vídeos</span>
+            </div>
+            {/* RIGHT */}
+            <div className='p-3'>
+              <ArrowRight className='text-gray-3 w-8 h-8 group-hover:text-gray-2 transition-all duration-300' />
+            </div>
+          </div>
+          
+          <div 
+            className='flex items-center justify-between p-2 group hover:scale-105 transition-all duration-300 sm:min-w-[50%]'
+            style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}
+          >
+            {/* LEFT */}
+            <div className='flex items-center gap-6'>
+              <div className='p-3 w-fit group-hover:brightness-150 transition-all duration-300' style={{borderRadius: '1rem', border: '1px solid var(--gray-05, #1E293B)', background: 'var(--gray-06, #0F172A)'}}>
+                <MusicNotesSimple className='text-gray-3 h-8 w-8'/>
+              </div>
+              <span className='text-gray-2 sm:text-lg font-bold select-none'>Criador de áudios e musicas </span>
+            </div>
+            {/* RIGHT */}
+            <div className='p-3'>
+              <ArrowRight className='text-gray-3 w-8 h-8 group-hover:text-gray-2 transition-all duration-300' />
+            </div>
+          </div>
+        </div>
+
+        {/* LINE */}
+        <div className='bg-gray-4 w-full h-[0.0625rem]'></div>
+
+        {/* INPUT */}
+        <form className='flex items-center justify-between sm:gap-2'>
+          <div 
+            className='flex items-center gap-2 sm:gap-3 p-2 max-w-[63.5rem] transition-all duration-300'
+            style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}
+          >
+            <PlusCircle className='w-8 h-8 text-gray-3 hover:scale-105 hover:brightness-150 transition-all duration-300'/>
+            <input 
+              type="text" 
+              placeholder='Envie uma mensagem ou digite “/” para exibir os comandos ' 
+              className='placeholder:text-gray-2 text-gray-2 bg-gray-6 focus:outline-none w-[70vw] sm:text-base text-xs group'
+            />
+          </div>
+          <div className='p-2 hover:scale-105 hover:brightness-150 transition-all duration-300' style={{borderRadius: '1rem', border: '1px solid var(--gray-04, #334155)', background: 'var(--gray-06, #0F172A)'}}>
+            <PaperPlaneRight className='text-gray-1 w-8 h-8' />
+          </div>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: 'var(--font-sans)'
+      },
+      colors: {
+        'gray-1': '#F8FAFC',
+        'gray-2': '#E2E8F0',
+        'gray-3': '#94A3B8',
+        'gray-4': '#334155',
+        'gray-5': '#1E293B',
+        'gray-6': '#0F172A',
+        'gray-7': '#020617',
+      }
     },
   },
   plugins: [],
